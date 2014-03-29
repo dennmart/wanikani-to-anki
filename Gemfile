71f3d342f2e7cad2ffdaa50e3f3df072f349ff2e
Dennis Martinez <dennis@dennmart.com>
2014-03-28 20:03:17 -0700

    Initial commit :smile_cat:

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'padrino', '0.12.0'
+gem 'rake'
+gem 'slim'
+gem 'wanikani'
+
+group :test do
+  gem 'rspec'
+  gem 'rack-test', require: 'rack/test'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+RACK_ENV = 'test' unless defined?(RACK_ENV)
+require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
+
+RSpec.configure do |conf|
+  conf.include Rack::Test::Methods
+end
+
+# You can use this method to custom specify a Rack app
+# you want rack-test to invoke:
+#
+#   app Wkanki::App
+#   app Wkanki::App.tap { |a| }
+#   app(Wkanki::App) do
+#     set :foo, :bar
+#   end
+#
+def app(app = nil, &blk)
+  @app ||= block_given? ? app.instance_eval(&blk) : app
+  @app ||= Padrino.application
+end
