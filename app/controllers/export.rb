acef038fc3f801b76698a722af76fc17d2727fde
Dennis Martinez <dennis@dennmart.com>
2014-03-29 15:21:10 -0700

    Redirect from export page if session is not set

--- app/controllers/export.rb
+++ app/controllers/export.rb
@@ -12,6 +12,8 @@ Wkanki::App.controllers :export do
   end
 
   get :index, map: '/export' do
+    redirect url(:home, :index) if session[:wanikani_api_key].blank?
+
     Wanikani.api_key = session[:wanikani_api_key]
     @wanikani_user = Wanikani::User.information
     render 'export/index'
--- spec/app/controllers/export_controller_spec.rb
+++ spec/app/controllers/export_controller_spec.rb
@@ -38,6 +38,12 @@ describe "ExportController" do
       Wanikani::User.stub(:information).and_return({ "username" => "dennmart" })
     end
 
+    it "redirects to the home page if the session key is not set" do
+      get '/export', {}, { "rack.session" => {} }
+      last_response.should be_redirect
+      last_response.location.should == "http://example.org/"
+    end
+
     it "sets the Wanikani.api_key from the session" do
       get '/export', {}, api_key_session
       Wanikani.api_key.should == "valid-api-key"
