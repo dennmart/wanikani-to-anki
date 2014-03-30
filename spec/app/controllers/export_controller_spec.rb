8dab1854e1ca9bcb0234a487010e26c796e06391
Dennis Martinez <dennis@dennmart.com>
2014-03-30 16:03:09 -0700

    Added specs for '/export/generate'

--- spec/app/controllers/export_controller_spec.rb
+++ spec/app/controllers/export_controller_spec.rb
@@ -19,4 +19,33 @@ describe "ExportController" do
       Wanikani.api_key.should == "valid-api-key"
     end
   end
+
+  describe "/export/generate" do
+    let(:empty_deck) { double(generate_deck: nil) }
+    let(:populated_deck) { double(generate_deck: "了; りょう - finish, complete, end") }
+
+    it "creates a new instance of AnkiDeck with the deck_type parameters" do
+      AnkiDeck.should_receive(:new).with("critical").and_return(empty_deck)
+      get "/export/generate/critical"
+    end
+
+    it "calls AnkiDeck#generate_deck with the created AnkiDeck object" do
+      AnkiDeck.stub(:new).and_return(empty_deck)
+      empty_deck.should_receive(:generate_deck)
+      get "/export/generate/critical"
+    end
+
+    it "redirects to /export if the deck is nil" do
+      AnkiDeck.stub(:new).and_return(empty_deck)
+      get "/export/generate/critical"
+      last_response.should be_redirect
+      last_response.location.should == "http://example.org/export"
+    end
+
+    it "prepares a plain text file ready to download" do
+      AnkiDeck.stub(:new).and_return(populated_deck)
+      get "/export/generate/critical"
+      last_response["Content-Type"].should == "text/plain;charset=utf-8"
+    end
+  end
 end
