ab7b5d279af975cd81575f9ecac2d55c16315209
Dennis Martinez <dennis@dennmart.com>
2014-04-02 07:07:00 -0700

    Added simple Javascript for toggling additional options

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,6 @@
-// Put your application scripts here
+$(document).ready(function() {
+  $(".additional-options-toggle").on("click", function(e) {
+    e.preventDefault();
+    $("#" + $(this).data("export-section") + "_options").toggle();
+  });
+});
