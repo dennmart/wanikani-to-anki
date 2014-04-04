f53c9874122d7128068c42d98434bdde675b9e03
Dennis Martinez <dennis@dennmart.com>
2014-04-04 07:33:31 -0700

    Adding basic error handling and Javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,4 +3,33 @@ $(document).ready(function() {
     e.preventDefault();
     $("#" + $(this).data("export-section") + "_options").toggle();
   });
+
+  $("#kanji_form input[name='argument']").on("click", function() {
+    $("#kanji_form input[id='selected_levels_specific']").attr('checked', 'checked');
+  });
+
+  $("#vocabulary_form input[name='argument']").on("click", function() {
+    $("#vocabulary_form input[id='selected_levels_specific']").attr('checked', 'checked');
+  });
+
+  $("#radicals_form input[name='argument']").on("click", function() {
+    $("#radicals_form input[id='selected_levels_specific']").attr('checked', 'checked');
+  });
+
+  $("#critical_items_form").validate({
+    rules: {
+      argument: {
+        required: false,
+        range: [1, 99]
+      }
+    },
+    messages: {
+      argument: "Please enter a value between 1 and 99, or leave blank for a default of 75."
+    }
+  });
+
+  $("#kanji_form input[name='argument']").on("click", function() {
+    $("#kanji_form input[id='selected_levels_specific']").attr('checked', 'checked');
+  });
+
 });
