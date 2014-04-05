6d5b33fbcc525954d054212bbd9c84f2ef6670a6
Dennis Martinez <dennis@dennmart.com>
2014-04-05 08:14:49 -0700

    Adding custom validation for level selection
    Need to DRY up this code desperately!

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
     $("#" + $(this).data("export-section") + "_options").toggle();
   });
 
+  jQuery.validator.addMethod("wanikaniLevelRange", function(value, element) {
+    return this.optional(element) || /^[0-9]+(,[0-9]+)*$/.test(value);
+  }, "Please use a list of comma-separated numbers (e.g. '1,10,25')");
+
   $("#kanji_form input[name='argument']").on("click", function() {
     $("#kanji_form input[id='selected_levels_specific']").prop('checked', true);
   });
@@ -25,6 +29,30 @@ $(document).ready(function() {
     },
     messages: {
       argument: "Please enter a value between 1 and 99, or leave blank for a default of 75."
+    }
+  });
+
+  $("#kanji_form").validate({
+    rules: {
+      argument: {
+        wanikaniLevelRange: true
+      }
+    }
+  });
+
+  $("#vocabulary_form").validate({
+    rules: {
+      argument: {
+        wanikaniLevelRange: true
+      }
+    }
+  });
+
+  $("#radicals_form").validate({
+    rules: {
+      argument: {
+        wanikaniLevelRange: true
+      }
     }
   });
 });
