719b03a3125375e30456fd8ab65aa8c70142ce4b
Dennis Martinez <dennis@dennmart.com>
2014-04-19 08:08:46 -0700

    Configuring Capistrano to use .env as a linked file

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,7 @@ set :scm, :git
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
+set :linked_files, %w{.env}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
