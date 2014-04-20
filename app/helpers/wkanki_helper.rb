81a57419d1623ec703c47607165ea24317d0af83
Dennis Martinez <dennis@dennmart.com>
2014-04-20 09:23:03 -0700

    Separating comments in generated decks into its own method

--- app/helpers/wkanki_helper.rb
+++ app/helpers/wkanki_helper.rb
@@ -20,10 +20,17 @@ module WkankiHelper
 
   def generate_anki_deck(type, cards)
     anki = Anki::Deck.new(card_data: cards)
+    deck = deck_comments_header(type)
+    deck += anki.generate_deck
+  end
+
+  private
+
+  def deck_comments_header(type)
     deck = "# WaniKani - #{type.chomp.sub(/./, &:upcase)}\n"
     deck += "# Generated on #{Time.now.strftime('%B %d, %Y %H:%M %p %Z')}\n"
     deck += "# WaniKani to Anki Exporter (http://wanikanitoanki.com)\n"
-    deck += anki.generate_deck
+    deck
   end
 end
 
