3f476c73cf451ad441f726327a909fdffff10270
Dennis Martinez <dennis@dennmart.com>
2015-02-25 19:46:51 -0800

    Added route to test Airbrake

--- app/controllers/home.rb
+++ app/controllers/home.rb
@@ -1,5 +1,9 @@
 Wkanki::App.controllers :home do
-  get :index, map: '/'  do
+  get :index, map: '/' do
     render 'home/index'
+  end
+
+  get :raise_error, map: '/raise_error' do
+    raise "Testing raised errors!"
   end
 end
