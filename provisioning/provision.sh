635a30a21d2e1ae7eeeb90296890649ba00c15f6
Dennis Martinez <dennis@dennmart.com>
2015-07-22 16:03:49 -0700

    Fixing provisioning script and remove silencing commands

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 echo "Updating apt..."
-sudo apt-get update -y -qq > /dev/null
+sudo apt-get update -y -qq
 
 echo "Installing required libraries for the app..."
-sudo apt-get -y -q install ruby-dev zlib1g-dev > /dev/null
+sudo apt-get -y -q install build-essential ruby-dev zlib1g-dev
 
 cd /vagrant
 
 echo "Installing bundler..."
-su -c "gem install bundler --no-rdoc --no-ri > /dev/null" root
+su -c "gem install bundler --no-rdoc --no-ri" root
 
 echo "Installing Ruby gems (this may take a while)..."
-bundle install --quiet > /dev/null
+bundle install
 
 echo "Starting up WaniKani To Anki..."
 rackup -D -o 0.0.0.0
