95f280bc47fd2012b9bd29d6db87f9116ab20b2e
Dennis Martinez <dennis@dennmart.com>
2015-10-17 16:47:35 -0700

    Updated provisioning script to use rbenv and Ruby 2.2.3
    The old script was using Ubuntu's default Ruby which is usually horribly
    outdated. This reflects what's running on the server.

--- Vagrantfile
+++ Vagrantfile
@@ -7,7 +7,5 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.network "forwarded_port", guest: 9292, host: 9393
-
-  # Provisioning is done by default via the shell.
-  config.vm.provision "shell", path: "provisioning/provision.sh"
+  config.vm.provision "shell", path: "provisioning/provision.sh", privileged: false
 end
--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env bash
-
 echo "Updating apt..."
 sudo apt-get update -y -qq
 
-echo "Installing required libraries for the app..."
-sudo apt-get -y -q install build-essential ruby-dev zlib1g-dev
+echo "Installing required libraries for Ruby..."
+sudo apt-get -y -qq install build-essential git libssl-dev libreadline-dev zlib1g-dev
 
-cd /vagrant
+echo "Setting up rbenv..."
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+echo "Installing Ruby 2.2.3 (this may take a while)..."
+rbenv install 2.2.3
+rbenv global 2.2.3
 
 echo "Installing bundler..."
-su -c "gem install bundler --no-rdoc --no-ri" root
+gem install bundler --no-rdoc --no-ri
 
 echo "Installing Ruby gems (this may take a while)..."
+cd /vagrant
 bundle install
 
 echo "Starting up WaniKani To Anki..."
 rackup -D -o 0.0.0.0
+
+echo "Done! You should be able to access WaniKani to Anki at http://localhost:9393. Enjoy!"
