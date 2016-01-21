ba39ef9ff466440df0c1555627667f6146a15cfc
Dennis Martinez <dennis@dennmart.com>
2016-01-21 16:55:34 +0900

    No need to use dotenv anymore
    The app is using an environment where variables are already set, so no
    need for this.

--- config/boot.rb
+++ config/boot.rb
@@ -5,9 +5,6 @@ PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
 # Load our dependencies
 require 'rubygems' unless defined?(Gem)
 
-require 'dotenv'
-Dotenv.load
-
 require 'bundler/setup'
 Bundler.require(:default, RACK_ENV)
 
