5b793c354a7258488b76a9e4f55b9779805b5e93
Dennis Martinez <dennis@dennmart.com>
2017-08-25 14:41:51 +0900

    Need to require a few more activesupport classes

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,7 @@
 require 'active_support/core_ext/module/aliasing'
 require 'active_support/core_ext/hash/reverse_merge'
+require 'active_support/core_ext/hash/keys'
+require 'active_support/core_ext/array/extract_options'
 
 module Wkanki
   class App < Padrino::Application
