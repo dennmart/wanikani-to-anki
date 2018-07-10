2b7353369c371fb49e1eb6164dd26ea8f7c53406
Dennis Martinez <dennis@dennmart.com>
2018-07-10 10:21:45 +0900

    Upgrade outdated gems

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 ruby '2.5.1'
 
 gem 'activesupport', '< 5'
-gem 'airbrake', '~> 7.2.1'
+gem 'airbrake', '~> 7.3.4'
 gem 'anki', '0.0.4'
 gem 'dalli', '~> 2.7.8'
 gem 'padrino', '0.14.3'
@@ -15,7 +15,7 @@ gem 'wanikani', '~> 2.0.0'
 gem 'yui-compressor', '~> 0.12.0', require: 'yui/compressor'
 
 group :test do
-  gem 'capybara', '~> 3.0.2'
+  gem 'capybara', '~> 3.3.1'
   gem 'rack-test', '~> 1.0.0', require: 'rack/test'
   gem 'rspec', '~> 3.7.0'
 end
--- app/app.rb
+++ app/app.rb
@@ -20,7 +20,7 @@ module Wkanki
 
       Airbrake.configure do |config|
         config.project_key         = ENV['WKANKI_AIRBRAKE_API'] || 'airbrake-api-key'
-        config.project_id          = ENV['WKANKI_AIRBRAKE_PROJECT_ID'] || 'airbrake-project-id'
+        config.project_id          = ENV['WKANKI_AIRBRAKE_PROJECT_ID'] || 123456
         config.host                = ENV['WKANKI_AIRBRAKE_HOST'] || 'https://test.airbrake.io/'
         config.environment         = ENV['RACK_ENV'] || 'development'
         config.ignore_environments = %w(development test)
